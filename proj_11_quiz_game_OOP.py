cd6688ae8bcbb6ae87549df972515a189e8059ed
Keyhan Kouhkiloui <kkouhkil@ed.ac.uk>
2023-03-07 13:35:25 +0000

    Quiz Game OOP - In Progress

--- proj_11_quiz_game_OOP.py
+++ proj_11_quiz_game_OOP.py
@@ -1,3 +1,7 @@
+"""Author: Keyhan Kouhkiloui Babarahmati
+           PhD in Robotics and AI
+"""
+
 question_data = [
     {"text": "A slug's blood is green.", "answer": "True"},
     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
